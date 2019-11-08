d06300e927a24ceb37bc2012964f99125ec2403d
Kal-Elx <kallej1johansson@gmail.com>
2019-11-08 21:51:35 +0100

    Added abstract game object class

---
+++ FallGameJam2019/app/src/main/java/fall_game_jam_2019/fallgamejam2019/GameObject.kt
@@ -0,0 +1,36 @@
+package fall_game_jam_2019.fallgamejam2019
+
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.Canvas
+
+abstract class GameObject(var image: Bitmap) {
+    var x: Int = 0
+    var y: Int = 0
+    var w: Int = 0
+    var h: Int = 0
+    protected var xVelocity = 20
+    protected var yVelocity = 20
+    protected val screenWidth = Resources.getSystem().displayMetrics.widthPixels
+    protected val screenHeight = Resources.getSystem().displayMetrics.heightPixels
+
+    init {
+        w = image.width
+        h = image.height
+
+        x = screenWidth/2
+        y = screenHeight/2
+    }
+
+    /**
+     * Draws the object on to the canvas.
+     */
+    fun draw(canvas: Canvas) {
+        canvas.drawBitmap(image, x.toFloat(), y.toFloat(), null)
+    }
+
+    /**
+     * update properties for the game object
+     */
+    abstract fun update()
+}
--- FallGameJam2019/app/src/main/java/fall_game_jam_2019/fallgamejam2019/Grenade.kt
+++ FallGameJam2019/app/src/main/java/fall_game_jam_2019/fallgamejam2019/Grenade.kt
@@ -10,35 +10,11 @@ import android.graphics.Canvas
  * Grenade is the opponent.
  */
 
-class Grenade(var image: Bitmap) {
-    var x: Int = 0
-    var y: Int = 0
-    var w: Int = 0
-    var h: Int = 0
-    private var xVelocity = 20
-    private var yVelocity = 20
-    private val screenWidth = Resources.getSystem().displayMetrics.widthPixels
-    private val screenHeight = Resources.getSystem().displayMetrics.heightPixels
-
-    init {
-        w = image.width
-        h = image.height
-
-        x = screenWidth/2
-        y = screenHeight/2
-    }
-
-    /**
-     * Draws the object on to the canvas.
-     */
-    fun draw(canvas: Canvas) {
-        canvas.drawBitmap(image, x.toFloat(), y.toFloat(), null)
-    }
-
+class Grenade(var img: Bitmap) : GameObject(img) {
     /**
      * update properties for the game object
      */
-    fun update() {
+    override fun update() {
         // val randomNum = ThreadLocalRandom.current().nextInt(1, 5)
 
         if (x > screenWidth - image.width || x < image.width) {
