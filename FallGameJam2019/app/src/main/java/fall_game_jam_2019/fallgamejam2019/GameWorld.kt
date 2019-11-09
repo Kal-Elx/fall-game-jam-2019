34f3d1e05f022132a0d262682278bd495d7eab87
Kal-Elx <kallej1johansson@gmail.com>
2019-11-09 19:56:04 +0100

    merge

--- FallGameJam2019/app/src/main/java/fall_game_jam_2019/fallgamejam2019/GameWorld.kt
+++ FallGameJam2019/app/src/main/java/fall_game_jam_2019/fallgamejam2019/GameWorld.kt
@@ -12,11 +12,15 @@ class GameWorld(resources: Resources) {
     var earth: Earth = Earth()
     var moon: Moon = Moon()
 
+    val G: Double = 6.67408 * 10.0.pow(-11)
+
     fun update(delta_time: Double){
 
         var moon_v2 = (moon.xVel.pow(2) + moon.yVel.pow(2))
         var moon_r = distance(moon.x, moon.y,earth.x,earth.y)
         var earth_g = moon.mass/ (moon_v2/moon_r)
         var moon_a_towards_earth = earth_g / moon.mass
+
+        val a = G*(earth.mass/distance(moon.x, moon.y,earth.x,earth.y).pow(2))
     }
 }
