12cd252c7836474de1d703ef01a6fbdbee01ba3e
Kal-Elx <kallej1johansson@gmail.com>
2019-11-10 11:40:17 +0100

    Added interface astronomical object

--- FallGameJam2019/app/src/main/java/fall_game_jam_2019/fallgamejam2019/GameWorld.kt
+++ FallGameJam2019/app/src/main/java/fall_game_jam_2019/fallgamejam2019/GameWorld.kt
@@ -1,7 +1,7 @@
 package fall_game_jam_2019.fallgamejam2019
 
 import android.content.res.Resources
-import kotlin.math.pow
+import kotlin.math.*
 
 class GameWorld(resources: Resources) {
 
@@ -9,10 +9,25 @@ class GameWorld(resources: Resources) {
     var earth: Earth = Earth()
     var moon: Moon = Moon()
 
+    init {
+        moon.yVel *= 10
+    }
+
     val G: Double = 6.67408 * 10.0.pow(-11)
-    val playbackSpeed = 1000 // Simulated seconds in one sec
+    val playbackSpeed = 100 // Simulated seconds in one sec
 
     fun update(fps: Int) {
+        // Calculate the moon´s acceleration towards the earth.
+        val Fg = -G*(earth.mass*moon.mass/moon.radius.pow(2))
+        val v = atan2(moon.y, moon.x)
+        val Fgx = Fg*cos(v)
+        val Fgy = Fg*sin(v)
+        val Ax = Fgx/moon.mass
+        val Ay = Fgy/moon.mass
+
+        moon.xVel += Ax*(playbackSpeed/fps)
+        moon.yVel += Ay*(playbackSpeed/fps)
+
         // Move moon
         moon.x += moon.xVel*playbackSpeed // delta time looks weird. Use FPS instead?
         moon.y += moon.yVel*playbackSpeed
