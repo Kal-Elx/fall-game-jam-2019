5bed23a507e80ece1118a4ed5bcb46a8dc8f213f
Kal-Elx <kallej1johansson@gmail.com>
2019-11-13 12:25:32 +0100

    Updated formulas.

--- FallGameJam2019/app/src/main/java/fall_game_jam_2019/fallgamejam2019/GameWorld.kt
+++ FallGameJam2019/app/src/main/java/fall_game_jam_2019/fallgamejam2019/GameWorld.kt
@@ -53,8 +53,8 @@ class GameWorld(resources: Resources) {
     }
 
     fun affectByGravity(target: AstronomicalObject, source: AstronomicalObject) {
-        val a = G*(source.mass / distance(target.x, target.y, source.x, source.y).pow(2))
-        val theta = atan2(source.y-target.y, source.x-target.x)
+        val a = -G*(source.mass / distance(target.x, target.y, source.x, source.y).pow(2))
+        val theta = atan2(target.y-source.y, target.x-source.x)
         val ax = a * cos(theta)
         val ay = a * sin(theta)
 
